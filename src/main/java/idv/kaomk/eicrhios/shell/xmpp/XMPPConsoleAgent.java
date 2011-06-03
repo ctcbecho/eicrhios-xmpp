b0e3099409d1778abe9ae47a1c126b86d1bbfd37
ctcbecho <ctcbecho@gmail.com>
2011-06-03 17:35:16 +0800

    Closes #1

--- src/main/java/idv/kaomk/eicrhios/shell/xmpp/XMPPConsoleAgent.java
+++ src/main/java/idv/kaomk/eicrhios/shell/xmpp/XMPPConsoleAgent.java
@@ -1,28 +1,30 @@
 package idv.kaomk.eicrhios.shell.xmpp;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FilterOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
+import jline.UnsupportedTerminal;
+
 import org.apache.felix.service.command.CommandProcessor;
 import org.apache.felix.service.command.CommandSession;
 import org.apache.karaf.shell.console.jline.Console;
-import org.apache.karaf.webconsole.gogo.Terminal;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManager;
 import org.jivesoftware.smack.ChatManagerListener;
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.RosterListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
@@ -33,8 +35,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class XMPPConsoleAgent {
-	public static final int TERM_WIDTH = 120;
-	public static final int TERM_HEIGHT = 39;
 
 	private Logger logger = LoggerFactory.getLogger(XMPPConsoleAgent.class);
 
@@ -46,9 +46,60 @@ public class XMPPConsoleAgent {
 	private String mHttpProxyHost;
 	private int mHttpProxyPort;
 	private Connection mXMPPConnection;
-	private CommandProcessor commandProcessor;
+	private CommandProcessor mCommandProcessor;
+	private String[] mAllowUsers;
+	private String karafName = "root";
+	private String consoleUser = "karaf";
+
 	private Map<String, SessionTerminal> mSessionMap = new Hashtable<String, SessionTerminal>();
-	private boolean isWaiting;
+
+	private void syncRosterEntries() throws XMPPException {
+		if (mXMPPConnection != null) {
+			Roster roster = mXMPPConnection.getRoster();
+			List<String> userList = mAllowUsers == null ? new ArrayList<String>()
+					: new ArrayList<String>(Arrays.asList(mAllowUsers));
+			List<RosterEntry> deleteList = new ArrayList<RosterEntry>();
+			for (RosterEntry re : roster.getEntries()) {
+				String user = re.getUser();
+				if (userList.contains(user)) {
+					logger.debug(String.format("user: %s already in roster",
+							user));
+					userList.remove(user);
+				} else {
+					deleteList.add(re);
+				}
+			}
+
+			for (RosterEntry re : deleteList) {
+				logger.debug(String.format("user: %s remove from roster",
+						re.getUser()));
+				roster.removeEntry(re);
+			}
+
+			for (String user : userList) {
+				if (user.length() > 0) {
+					logger.debug(String.format("user: %s add to roster", user));
+					roster.createEntry(user, user, new String[0]);
+				}
+
+			}
+
+		}
+
+	}
+
+	public void disconnect() throws XMPPException {
+		if (mXMPPConnection != null && mXMPPConnection.isConnected()) {
+			mXMPPConnection.disconnect();
+			logger.debug("XMPPConnection disconnect successfully");
+
+			for (Map.Entry<String, SessionTerminal> entry : mSessionMap
+					.entrySet()) {
+				entry.getValue().close();
+			}
+			mSessionMap.clear();
+		}
+	}
 
 	public void connect() throws XMPPException {
 		if (mXMPPConnection != null && mXMPPConnection.isConnected()) {
@@ -95,37 +146,32 @@ public class XMPPConsoleAgent {
 
 			@Override
 			public void presenceChanged(Presence presence) {
-				logger.debug(String.format("presenceChanged: %s %s",
+				logger.debug(String.format(
+						"RosterListener.presenceChanged: %s %s",
 						presence.getFrom(), presence.getType()));
 
 				if (!presence.isAvailable()
 						&& mSessionMap.containsKey(presence.getFrom())) {
-					mSessionMap.remove(presence.getFrom()).closed();
+					mSessionMap.remove(presence.getFrom()).close();
 				}
 			}
 
 			@Override
 			public void entriesUpdated(Collection<String> arg0) {
-				// TODO Auto-generated method stub
 
 			}
 
 			@Override
 			public void entriesDeleted(Collection<String> arg0) {
-				// TODO Auto-generated method stub
 
 			}
 
 			@Override
 			public void entriesAdded(Collection<String> arg0) {
-				// TODO Auto-generated method stub
 
 			}
 		});
 
-		mXMPPConnection.sendPacket(new Presence(Presence.Type.available));
-
-		// Assume we've created a Connection name "connection".
 		ChatManager chatmanager = mXMPPConnection.getChatManager();
 
 		chatmanager.addChatListener(new ChatManagerListener() {
@@ -136,6 +182,7 @@ public class XMPPConsoleAgent {
 						@Override
 						public void processMessage(Chat chat, Message message) {
 							try {
+								StringBuilder sb = new StringBuilder();
 								SessionTerminal st = mSessionMap.get(message
 										.getFrom());
 
@@ -144,35 +191,26 @@ public class XMPPConsoleAgent {
 									mSessionMap.put(message.getFrom(), st);
 								}
 
-								if (message.getType() == Message.Type.error) {
+								switch (message.getType()) {
+								case chat:
+									sb.append(message.getBody()).append("\n");
+									st.write(sb.toString());
+									break;
+								case error:
 									logger.warn(String.format(
 											"receive error xmpp essage: %s",
 											message.toXML()));
-								} else if (message.getType() == Message.Type.chat) {
-									synchronized (st) {
-										if (isWaiting) {
-											st.notify();
-											isWaiting = false;
-											return;
-										}
-									}
-									StringBuilder sb = new StringBuilder();
-									sb.append(message.getBody()).append("\n");
-									String str = message.getBody();
-									try {
-										st.handle(sb.toString());
-									} catch (IOException e) {
-										logger.error("error during handle: ", e);
-									}
-									//
-									// chat.sendMessage("11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"+
-									// "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
-								} else
+									chat.sendMessage("\n xmpp-error: "
+											+ message.getError().getCondition());
+									break;
+								default:
 									throw new RuntimeException(
 											"shoud not happen!!");
 
+								}
 							} catch (Exception e) {
-								throw new RuntimeException(e);
+								throw new RuntimeException(
+										"error during processMessage: ", e);
 							}
 
 						}
@@ -182,9 +220,9 @@ public class XMPPConsoleAgent {
 		});
 
 		mXMPPConnection.login(mUsername, mPassword);
+		logger.debug("XMPPConnection login successfully.");
+		syncRosterEntries();
 
-		logger.debug("mXMPPConnection login successfully.");
-		
 	}
 
 	public String getHost() {
@@ -244,7 +282,28 @@ public class XMPPConsoleAgent {
 	}
 
 	public void setCommandProcessor(CommandProcessor commandProcessor) {
-		this.commandProcessor = commandProcessor;
+		this.mCommandProcessor = commandProcessor;
+	}
+
+	public void setAllowUsers(String allowUsers) {
+		mAllowUsers = allowUsers.split(",");
+	}
+
+	
+	public String getKarafName() {
+		return karafName;
+	}
+
+	public void setKarafName(String karafName) {
+		this.karafName = karafName;
+	}
+
+	public String getConsoleUser() {
+		return consoleUser;
+	}
+
+	public void setConsoleUser(String consoleUser) {
+		this.consoleUser = consoleUser;
 	}
 
 	@Override
@@ -257,86 +316,73 @@ public class XMPPConsoleAgent {
 
 	public class SessionTerminal implements Runnable {
 
-		private Terminal terminal;
-		private Console console;
-		private PipedOutputStream in;
-		private PipedInputStream out;
-		private boolean closed;
+		private Console mConsole;
+		private PipedOutputStream mConsoleInputStream;
+		private PipedInputStream mConsoleOutputStream;
+		private boolean mClosed;
 		private Chat mChat;
 		private List<String> mLineBuffer = new ArrayList<String>();
-		private boolean mShouleIgnoreMessage = false;
-		private int mMsgCount = 0;
+
 		private int mMessageLength = 0;
 
 		public SessionTerminal(Chat chat) throws IOException {
 			try {
 				this.mChat = chat;
-				this.terminal = new Terminal(TERM_WIDTH, TERM_HEIGHT);
-				terminal.write("\u001b\u005B20\u0068"); // set newline mode on
-
-				in = new PipedOutputStream();
-				out = new PipedInputStream();
+				mConsoleInputStream = new PipedOutputStream();
+				mConsoleOutputStream = new PipedInputStream();
 				PrintStream pipedOut = new PrintStream(new PipedOutputStream(
-						out), true);
+						mConsoleOutputStream), true);
 
-				console = new Console(commandProcessor,
-						new PipedInputStream(in), pipedOut, pipedOut,
-						new XMPPTerminal(), null);
-				CommandSession session = console.getSession();
+				mConsole = new Console(mCommandProcessor, new PipedInputStream(
+						mConsoleInputStream), pipedOut, pipedOut,
+						new UnsupportedTerminal(), null);
+
+				CommandSession session = mConsole.getSession();
 				session.put("APPLICATION",
-						System.getProperty("karaf.name", "root"));
-				session.put("USER", "karaf");
-				session.put("COLUMNS", Integer.toString(TERM_WIDTH));
-				session.put("LINES", Integer.toString(TERM_HEIGHT));
+						System.getProperty("karaf.name", karafName));
+				session.put("USER", consoleUser);
+
 			} catch (IOException e) {
-				e.printStackTrace();
 				throw e;
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw (IOException) new IOException().initCause(e);
 			}
-			new Thread(console).start();
+			new Thread(mConsole).start();
 			new Thread(this).start();
 		}
 
 		public boolean isClosed() {
-			return closed;
+			return mClosed;
 		}
 
-		public void closed() {
-			console.close();
-			closed = true;
+		public void close() {
+			mConsole.close();
+			mClosed = true;
 		}
 
-		public void handle(String str) throws IOException {
+		public void write(String str) throws IOException {
 			try {
 				if (str != null && str.length() > 0) {
-					String d = terminal.pipe(str);
-					for (byte b : d.getBytes()) {
-						in.write(b);
-					}
-					in.flush();
+					mConsoleInputStream.write(str.getBytes());
+					mConsoleInputStream.flush();
 				}
-
 			} catch (IOException e) {
-				closed = true;
+				close();
 				throw e;
 			}
 		}
 
 		private void addLine(String line) throws Exception {
-			line = line.replaceAll("\u001B\\[[\\d;]+m", "");
+			line = line.replaceAll("\u001B\\[[\\d;]*m", "");
 
-			synchronized (this) {
-				if (mMessageLength + line.length() > 1024) {
-					flushLines();
-					isWaiting = true;
-					wait();
-
-				}
-				mLineBuffer.add(line);
-				mMessageLength += line.length();
+			if (mMessageLength + line.length() > 1024) {
+				flushLines();
 			}
+
+			mLineBuffer.add(line);
+			mMessageLength += line.length();
+
 		}
 
 		private void flushLines() throws XMPPException {
@@ -349,16 +395,8 @@ public class XMPPConsoleAgent {
 
 			if (sb.length() > 0) {
 				try {
-					System.out.println("length:" + sb.length());
 					mChat.sendMessage(sb.toString());
-					// mMsgCount++;
-					//
-					// if (mMsgCount >= 10) {
-					// mChat.sendMessage("\nMesage is too long..");
-					// mShouleIgnoreMessage = true;
-					// console.close();
-					//
-					// }
+
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
@@ -372,12 +410,12 @@ public class XMPPConsoleAgent {
 				int retryTimes = 0;
 				int matchedPos = 0;
 				byte[] buffer = new byte[256];
-				byte[] prompt = "root> ".getBytes();
+				byte[] prompt = (karafName + "> ").getBytes();
 				ByteArrayOutputStream output = new ByteArrayOutputStream();
 
 				readData: for (;;) {
-					while (out.available() == 0) {
-						if (retryTimes > 6 * 100) { // 30 mins
+					while (mConsoleOutputStream.available() == 0) {
+						if (retryTimes > 6 * 100) {
 							addLine(output.toString());
 							flushLines();
 							output = new ByteArrayOutputStream();
@@ -392,7 +430,7 @@ public class XMPPConsoleAgent {
 					}
 
 					logger.debug("start to read console...");
-					bytesRead = out.read(buffer);
+					bytesRead = mConsoleOutputStream.read(buffer);
 					logger.debug(String.format("read console %d bytes",
 							bytesRead));
 
@@ -422,14 +460,14 @@ public class XMPPConsoleAgent {
 							addLine(output.toString());
 							flushLines();
 							output = new ByteArrayOutputStream();
-							mShouleIgnoreMessage = false;
-							mMsgCount = 0;
+
 						}
 					}
 				}
 			} catch (Exception e) {
-				closed = true;
-				e.printStackTrace();
+				logger.error("error during console reading: ", e);
+				close();
+				
 			}
 		}
 
